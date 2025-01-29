51438b04dfb297e155830867506cb46b262aa280
joeheartless <114878956+joeheartless@users.noreply.github.com>
2025-01-29 23:43:34 +0700

    Update bunda-euis.sh

--- bunda-euis.sh
+++ bunda-euis.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 # BUNDA-EUIS File Encryptor v.1
 # Created by Prima Setiawan / joeheartless
-#
-file=$(ls -f *.*)
+
+read -sp "Enter password: " pass
+echo ""
+
+# Dapetin semua file kecuali script dan file yang udah terenkripsi
+file_list=$(find . -maxdepth 1 -type f -not -name "bunda.*" -not -name "*.sh")
+
 i=1
-for enc in $file 
-do 
-	echo "[$i] File $enc encrypted"
-	openssl enc -aes-256-cbc -in $enc -out bunda.$enc -pbkdf2 -k bundaeuisgeulis # <-- $passwed
-	file[i]=$enc
-	i=$(( i + 1 ))
+for enc in $file_list; do 
+    echo "[$i] File $enc encrypted"
+    openssl enc -aes-256-cbc -in "$enc" -out "bunda.$enc" -pbkdf2 -k "$pass"
+    
+    # Hapus file original setelah dienkripsi
+    rm -f "$enc"
+    
+    i=$((i + 1))
 done
-rm -rf $file
+
+# Tambahin README buat info
 echo "File encrypted, use Bunda-Euis-Decryptor to decrypt." > README.txt
-sleep 1
 echo "--------------------------------------------------------------------------------------------------------------------------------------"
 ls
+
